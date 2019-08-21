b3aa1b0384459a944e37473cbf285771f41096f5
Connor Shea <connor.james.shea@gmail.com>
2019-08-20 22:23:35 -0600

    Add CursedRbiPlugin

--- config/initializers/sorbet_rails.rb
+++ config/initializers/sorbet_rails.rb
@@ -2,6 +2,8 @@
 require Rails.root.join('lib', 'kaminari_rbi_plugin')
 require Rails.root.join('lib', 'pg_search_rbi_plugin')
 require Rails.root.join('lib', 'friendly_id_rbi_plugin')
+require Rails.root.join('lib', 'cursed_rbi_plugin')
 SorbetRails::ModelRbiFormatter.register_plugin(KaminariRbiPlugin)
 SorbetRails::ModelRbiFormatter.register_plugin(PgSearchRbiPlugin)
 SorbetRails::ModelRbiFormatter.register_plugin(FriendlyIdRbiPlugin)
+SorbetRails::ModelRbiFormatter.register_plugin(CursedRbiPlugin)
